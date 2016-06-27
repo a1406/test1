1fedd5af2d39c6316dd5d65e160f063935f6c4bc
jack at home <jack@home>
2016-06-27 23:40:27 +0800

    1

--- distinct_subsequences.c
+++ distinct_subsequences.c
@@ -3,8 +3,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+//把S变少
+//1 s[0] = t[0]
+//2 s[last] = t[last]
+//3 s中每个字符都是t中包含的
+char *reset_s(char *s, char *t)
+{
+	return s;
+}
+
+int numDistinct(char* s, char* t) {
+    s = reset_s(s, t);
+}
+
 int main(int argc, char *argv[])
 {
-	
+	int ret = numDistinct(argv[1], argv[2]);
+	printf("ret = %d\n", ret);
     return 0;
 }
