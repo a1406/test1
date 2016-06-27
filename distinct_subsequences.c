2d476673d197bb43b7d5c736e15589e1f5caaf9f
jack at home <jack@home>
2016-06-27 23:48:03 +0800

    1

--- distinct_subsequences.c
+++ distinct_subsequences.c
@@ -9,11 +9,39 @@
 //3 s中每个字符都是t中包含的
 char *reset_s(char *s, char *t)
 {
-	return s;
+	if (!t || !*t)
+		return NULL;
+	int len_t = strlen(t);
+	while (*s && *s != t[0]) {
+		++s;
+	}
+	if (!*s)
+		return NULL;
+	int len_s = strlen(s);
+	
+	char *ret = malloc(len_s);
+	char *p = ret;
+	int i;
+	while (*s) {
+		for (i = 0; i < len_t; i++) {
+			if (*s == t[i]) {
+				*p = *s;
+				++p;
+			}
+		}
+		++s;
+	}
+	while (*p != t[len_t - 1] && p != ret) {
+		*p = '\0';
+	}
+	if (p == ret)
+		return NULL;
+	return ret;
 }
 
 int numDistinct(char* s, char* t) {
     s = reset_s(s, t);
+	
 }
 
 int main(int argc, char *argv[])
