5eded4867c0605eea590d8c950ad6b88d710ae50
Your Name <you@example.com>
2016-06-27 19:40:49 +0800

    12

--- jump_game.c
+++ jump_game.c
@@ -10,6 +10,16 @@ int jump(int* nums, int numsSize) {
 
 int main(int argc, char *argv[])
 {
-    
+	int num_size = argc - 1;
+    int *nums = malloc(sizeof(int) * num_size);
+
+	int i;
+	for (i = 1; i < argc; i++)
+	{
+		nums[i - 1] = atoi(argv[i]);
+	}
+	
+	int ret = jump(nums, num_size);
+	printf("ret = %d\n", ret);
     return 0;
 }
