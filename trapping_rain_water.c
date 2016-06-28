c355f5c3af164a1b2ec19a24bc97dadc880e5c92
Your Name <you@example.com>
2016-06-28 20:23:09 +0800

    1

--- trapping_rain_water.c
+++ trapping_rain_water.c
@@ -2,8 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+enum trap_state
+{
+	INIT,
+	FIND_BOTTOM,
+	FIND_TOP_RIGHT,
+};
+
+typedef void (*trap_func)(int height);
+
+void init_func(int height)
+{
+}
+
+void find_bottom_func(int height)
+{
+}
+
+void find_top_right_func(int height)
+{
+}
+
+trap_func g_func;
+int g_ret;
+
 int trap(int* height, int heightSize) {
-	return (0);
+	g_func = init_func;
+	g_ret = 0;
+	int i;
+	for (i = 0; i < heightSize; ++i) {
+		g_func(height[i]);
+	}
+	return (g_ret);
 }
 
 int main(int argc, char *argv[])
