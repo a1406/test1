b3b6a4611a795dafebb05d448ae79443c5eadf5c
Your Name <you@example.com>
2016-06-28 20:40:23 +0800

    111

--- trapping_rain_water.c
+++ trapping_rain_water.c
@@ -9,20 +9,43 @@ enum trap_state
 	FIND_TOP_RIGHT,
 };
 
+void init_func(int *height);
+void find_bottom_func(int *height);
+void find_top_right_func(int *height);
+
 typedef void (*trap_func)(int *height);
 trap_func g_func;
 int g_ret;
+int left_max, real_max;
 
 void init_func(int *height)
 {
+	if (height[0] < height[-1]) {
+		g_func = find_bottom_func;
+		left_max = height[-1];
+	}
 }
 
 void find_bottom_func(int *height)
 {
+	if (height[0] < height[1]) {
+		g_func = find_top_right_func;
+	}
 }
 
 void find_top_right_func(int *height)
 {
+	if (height[0] >= left_max) {
+		g_func = init_func;
+		real_max = left_max;
+		return;
+	}
+	if (height[0] > height[1])
+	{
+		g_func = find_bottom_func;
+		left_max = height[0];
+		real_max = height[0];
+	}
 }
 
 int trap(int* height, int heightSize) {
