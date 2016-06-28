f429209f5437209e97103e215106284863216e28
Your Name <you@example.com>
2016-06-28 16:02:57 +0800

    1

--- edit_distance.c
+++ edit_distance.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 int minDistance(char* word1, char* word2) {
+	int len1 = strlen(word1);
+	int len2 = strlen(word2);	
 	return (0);
 }
 
