7986c6601130747fa594a4610c825de3d0c77711
Your Name <you@example.com>
2016-06-29 14:44:59 +0800

    1

--- text_justification.c
+++ text_justification.c
@@ -6,6 +6,7 @@
  *   * Note: The returned array must be malloced, assume caller calls free().
  *    */
 char** fullJustify(char** words, int wordsSize, int maxWidth, int* returnSize) {
+	*returnSize = 0;
 	return NULL;
 }
 
