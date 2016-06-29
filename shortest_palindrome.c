2b3a1b8e45bd9f788ecc44c51a76f20b0c37aa79
Your Name <you@example.com>
2016-06-29 19:51:17 +0800

    1

--- shortest_palindrome.c
+++ shortest_palindrome.c
@@ -7,13 +7,37 @@ int get_palindrome_index(int index, int size)
 	return size - 1 - index;
 }
 
+int get_mid_index(int size)
+{
+	return (size - 2) / 2;
+}
+
+char *alloc_palindrome(char *s, int len, int mid)
+{
+	return NULL;
+}
+
 
 char* shortestPalindrome(char* s) {
 	int len = strlen(s);
-	char *ret = malloc(len);
-	int mid = len / 2;
-	
-	return (ret);
+
+	int mid = get_mid_index(len);
+	int tmp_len = len;
+	while (mid > 0) {
+		int i;
+		for (i = 0; i < mid; ++i) {
+			int index = get_palindrome_index(i, tmp_len);
+			if (s[i] != s[index])
+				break;
+		}
+		if (i == mid) {
+			return alloc_palindrome(s, len, mid);
+		}
+		--tmp_len;
+		--mid;
+	}
+
+	return alloc_palindrome(s, len, mid);
 }
 
 int main(int argc, char *argv[])
