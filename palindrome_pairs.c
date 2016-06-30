1afa63bee5ba562a53e06d25b35bba2e7a505c5d
jack at home <jack@home>
2016-06-30 23:57:47 +0800

    sort

--- palindrome_pairs.c
+++ palindrome_pairs.c
@@ -35,6 +35,62 @@ bool check_palindrome(char *a, char *b)
 	return true;
 }
 
+char **g_words;
+static int comp1(const void *p1, const void *p2)
+{
+	char *w1 = g_words[*(int *)p1];
+	char *w2 = g_words[*(int *)p2];
+	while (*w1 && *w2) {
+		if (*w1 > *w2)
+			return 1;
+		if (*w2 > *w1)
+			return -1;
+		++w1;
+		++w2;
+	}
+
+	if (!*w1 && !*w2)
+		return 0;
+	if (!*w1)
+		return -1;
+	return 1;
+}
+
+static int comp2(const void *p1, const void *p2)
+{
+	char *w1 = g_words[*(int *)p1];
+	char *w2 = g_words[*(int *)p2];
+	int len1 = strlen(w1);
+	int len2 = strlen(w2);
+	int i;
+	for (i = 0; i < len1 && i < len2; i++) {
+		if (w1[len1 - i - 1] > w2[len2 - i - 1])
+			return 1;
+		if (w1[len1 - i - 1] < w2[len2 - i - 1])
+			return -1;		
+	}
+	if (len1 == len2)
+		return (0);
+	if (len1 > len2)
+		return 1;
+	return -1;
+}
+
+void sort_words(int size, int **sort1, int **sort2)
+{
+	int i;
+	*sort1 = malloc(sizeof(int) * size);
+	*sort2 = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++) {
+		(*sort1)[i] = i;
+		(*sort2)[i] = i;
+	}
+
+
+	qsort(*sort1, size, sizeof(int), comp1);
+	qsort(*sort2, size, sizeof(int), comp2);	
+}
+
 /**
  *  * Return an array of arrays of size *returnSize.
  *   * The sizes of the arrays are returned as *columnSizes array.
@@ -68,6 +124,25 @@ int** palindromePairs(char** words, int wordsSize, int** columnSizes, int* retur
 
 int main(int argc, char *argv[])
 {
+	int i;
+	g_words = &argv[1];
+	int *sort1, *sort2;
+	for (i = 0; i < argc - 1; i++) {
+		printf("%d: %s\n", i, argv[i+1]);
+	}
+	
+	sort_words(argc - 1, &sort1, &sort2);
+	for (i = 0; i < argc - 1; i++) {
+		printf("%d ", sort1[i]);
+	}
+	printf("\n");
+	for (i = 0; i < argc - 1; i++) {
+		printf("%d ", sort2[i]);
+	}
+	printf("\n");	
+	return (0);
+
+	
     if (check_palindrome(argv[1], argv[2]))
 		printf("true\n");
 	else
