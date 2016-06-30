7446711b828f2d8596c3dd71982032796cb1b4e3
Your Name <you@example.com>
2016-06-30 20:32:52 +0800

    1

--- palindrome_pairs.c
+++ palindrome_pairs.c
@@ -15,3 +15,14 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+
+/*
+Example 1:
+	Given words = ["bat", "tab", "cat"]
+	Return [[0, 1], [1, 0]]
+	The palindromes are ["battab", "tabbat"]
+Example 2:
+	Given words = ["abcd", "dcba", "lls", "s", "sssll"]
+	Return [[0, 1], [1, 0], [3, 2], [2, 4]]
+	The palindromes are ["dcbaabcd", "abcddcba", "slls", "llssssll"]
+*/
