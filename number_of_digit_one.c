2177d0b81b1d9bcd6033aeef732028a44af62477
Your Name <you@example.com>
2016-07-07 17:05:14 +0800

    1

--- number_of_digit_one.c
+++ number_of_digit_one.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+int is_have_one(int num)
+{
+	int i;
+	int ret = 0;
+	for (i = 0; i < 3; i++) {
+		if (num % 10 == 1)
+			++ret;
+		num = num / 10;
+	}
+	return ret;
+}
 
 int main(int argc, char *argv[])
 {
-    
+    int i;
+	int num = 0;
+	for (i = 0; i < 1000; i++) {
+		num += is_have_one(i);
+		printf("%d, ", num);
+	}
+	printf("\n");
+	
     return 0;
 }
